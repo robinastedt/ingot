b808e2ed2974461c7639c62c64ea5e121b04134c
Robin Åstedt <robin.astedt@gmail.com>
2021-11-16 21:14:49 +0100

    Fix compile script after restructure

--- compile.sh
+++ compile.sh
@@ -5,8 +5,8 @@ set -ex
 # Comment out executing JIT in Generator.cc before using
 # TODO: have ingotc parse arguments and either execute JIT or output LLVM IR.
 
-cmake --build build
-./build/ingotc ${1} > ${2}.ll
+cmake --build build -j16
+./build/ingot/ingotc ${1} > ${2}.ll
 llc ${2}.ll -o ${2}.s
 g++ -o ${2}.o -c ${2}.s
 g++ -o ${2} ${2}.o
